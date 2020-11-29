1c3e5095ba2455e2d69d5aa1aa222b132b6c5531
Monika <mon.fur89@gmail.com>
2020-11-29 23:59:19 +0100

    js

---
+++ calculator.js
@@ -0,0 +1,84 @@
+const btnone = document.querySelector("#one");
+btnone.addEventListener("click", function () {
+input.value += parseInt(btnone.innerHTML);
+});
+
+const btntwo = document.querySelector("#two");
+btntwo.addEventListener("click", function () {
+input.value += parseInt(btntwo.innerHTML);
+});
+
+const btnthree = document.querySelector("#three");
+btnthree.addEventListener("click", function () {
+input.value += parseInt(btnthree.innerHTML);
+});
+
+const btnfour = document.querySelector("#four");
+btnfour.addEventListener("click", function () {
+input.value += parseInt(btnfour.innerHTML);
+});
+
+const btnfive = document.querySelector("#five");
+btnfive.addEventListener("click", function () {
+input.value += parseInt(btnfive.innerHTML);
+});
+
+const btnsix = document.querySelector("#six");
+btnsix.addEventListener("click", function () {
+input.value += parseInt(btnsix.innerHTML);
+});
+
+const btnseven = document.querySelector("#seven");
+btnseven.addEventListener("click", function () {
+input.value += parseInt(btnseven.innerHTML);
+});
+
+const btneight = document.querySelector("#eight");
+btneight.addEventListener("click", function () {
+input.value += parseInt(btneight.innerHTML);
+});
+
+const btnnine = document.querySelector("#nine");
+btnnine.addEventListener("click", function () {
+input.value += parseInt(btnnine.innerHTML);
+});
+
+const btnzero = document.querySelector("#zero");
+btnzero.addEventListener("click", function () {
+input.value += parseInt(btnzero.innerHTML);
+});
+
+const btnminus = document.querySelector("#minus");
+btnminus.addEventListener("click", function () {
+input.value += btnminus.innerHTML;
+});
+
+const btnplus = document.querySelector("#plus");
+btnplus.addEventListener("click", function () {
+input.value += btnplus.innerHTML;
+});
+
+const btndivide = document.querySelector("#divide");
+btndivide.addEventListener("click", function () {
+input.value += btndivide.innerHTML;
+});
+
+const btnmultiply = document.querySelector("#multiply");
+btnmultiply.addEventListener("click", function () {
+input.value += btnmultiply.innerHTML;
+});
+
+const btnequal = document.querySelector("#equal");
+btnequal.addEventListener("click", function () {
+input.value += btnequal.innerHTML;
+});
+
+const btnpoint = document.querySelector("#point");
+btnpoint.addEventListener("click", function () {
+input.value += btnpoint.innerHTML;
+});
+
+const btnclear = document.querySelector("#clear");
+btnclear.addEventListener("click", function () {
+input.value = "";
+});
